4c47b69fb1136bca7b46931559244c0f196a08c3
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2023-12-18 10:22:56 +0900

    軽微な修正とmobileビューフォルダの新設

---
+++ for-mobile-view/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("../images"));
+app.use("/images", express.static("../images"));
+
+app.get("/", async function (req, res) {
+  res.sendFile("./index.html");
+});
+
+app.listen(8080, function () {
+  console.log("[NodeJS] Application Listening on Port 8080");
+});
