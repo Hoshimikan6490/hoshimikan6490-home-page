0480abab69cda736c8b6151b972e3e9ed60e6727
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-01-03 16:11:12 +0900

    スマホ対応に向け、調整

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+let port = 8080;
+
+app.use(express.static("./images"));
+app.use("/images", express.static("./images"));
+
+app.get("/", async function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, function () {
+  console.log(`[NodeJS] Application Listening on Port ${port}`);
+});
