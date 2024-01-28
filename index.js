72d3f57d62b1dec7930c0f011f1fbde98638ce0a
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-01-28 18:57:12 +0900

    パフォーマンス向上

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 let port = 8080;
 
+///圧縮ライブラリ compression を使用
+const compression = require("compression");
+
+// 全HTTPレスポンスでgzip配信有効化
+app.use(compression());
+
 app.use(express.static("./images"));
 app.use("/images", express.static("./images"));
 
