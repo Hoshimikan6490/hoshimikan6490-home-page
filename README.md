103d7847897df82ce991042e5f160afb2415fa41
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-06-04 22:25:38 +0900

    サイトアドレスの変更に伴う修正

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Please see this
-https://hoshimikan6490.github.io/about-me/
+https://hoshimikan6490.github.io/
--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const express = require("express");
 const app = express();
 let port = 8080;
 
-///圧縮ライブラリ compression を使用
-const compression = require("compression");
-
-// 全HTTPレスポンスでgzip配信有効化
-app.use(compression());
-
 app.use(express.static("./images"));
 app.use("/images", express.static("./images"));
 
 app.get("/", async function (req, res) {
-  res.redirect("/about-me")
-});
-
-app.get("/about-me", async function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
