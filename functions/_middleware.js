692fd1ebe949ce4037ba5bb04bbf80c80c7d34fb
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-07 16:37:33 +0900

    /markdown-share-button/用のリバースプロキシミドルウェアを追加

---
+++ functions/_middleware.js
@@ -0,0 +1,26 @@
+const handleReverseProxy = async (context) => {
+  const originalUrl = context.request.url;
+  const url = new URL(originalUrl);
+  // /markdown-share-button/内でなければ処理を中断
+  if (url.pathname.indexOf("/markdown-share-button/") !== 0) {
+    return await context.next();
+  }
+  // /markdown-share-button/内であればhttps://markdown-share-button.pages.devよりコンテンツを取得
+  const newUrl = new URL(
+    `https://markdown-share-button.pages.dev${url.pathname.replace(
+      "/markdown-share-button/",
+      "/"
+    )}${url.search}`
+  );
+  const response = await fetch(new Request(newUrl), {
+    headers: new Headers(context.request.headers),
+  });
+  // 取得したコンテンツをレスポンスとして返す
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: new Headers(response.headers),
+  });
+};
+
+export const onRequest = [handleReverseProxy];
