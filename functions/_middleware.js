e6955ed7b04d35bdb3b14127c6a3c5c1f53f611a
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-07 16:51:10 +0900

    bug fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,13 +2,13 @@ export async function onRequest(context) {
   const originalUrl = context.request.url;
   const url = new URL(originalUrl);
   // /markdown-share-button/内でなければ処理を中断
-  if (url.pathname.indexOf("/markdown-share-button/") !== 0) {
+  if (url.pathname.indexOf("/markdown-share-button") !== 0) {
     return await context.next();
   }
   // /markdown-share-button/内であればhttps://markdown-share-button.pages.devよりコンテンツを取得
   const newUrl = new URL(
     `https://markdown-share-button.pages.dev${url.pathname.replace(
-      "/markdown-share-button/",
+      "/markdown-share-button",
       "/"
     )}${url.search}`
   );
