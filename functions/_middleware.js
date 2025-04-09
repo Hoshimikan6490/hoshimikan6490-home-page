41207b504cc01c5564aef6b21d4abbf59c1feee4
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-04-09 13:12:09 +0900

    条件文を修正し、特定のパスに対する処理を中断するロジックを改善

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,13 +1,11 @@
 export async function onRequest(context) {
   const originalUrl = context.request.url;
   const url = new URL(originalUrl);
+
   // /markdown-share-button/内でなければ処理を中断
-  if (
-    !url.pathname.startsWith("/markdown-share-button") &&
-    url.pathname.startsWith("/markdown-share-button/markdown-share-button")
-  ) {
+  if (!url.pathname.startsWith("/markdown-share-button"))
     return await context.next();
-  }
+
   // /markdown-share-button/内であればhttps://markdown-share-button.pages.devよりコンテンツを取得
   const newUrl = new URL(
     `https://markdown-share-button.pages.dev${url.pathname.replace(
