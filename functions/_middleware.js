55ecdcd5707012d3c71e548989a747861e3c1473
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-08-15 23:19:01 +0900

    format

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,26 +1,26 @@
 export async function onRequest(context) {
-  const originalUrl = context.request.url;
-  const url = new URL(originalUrl);
+	const originalUrl = context.request.url;
+	const url = new URL(originalUrl);
 
-  // /markdown-share-button/内でなければ処理を中断
-  if (!url.pathname.startsWith("/markdown-share-button"))
-    return await context.next();
+	// /markdown-share-button/内でなければ処理を中断
+	if (!url.pathname.startsWith("/markdown-share-button"))
+		return await context.next();
 
-  // /markdown-share-button/内であればhttps://markdown-share-button.pages.devよりコンテンツを取得
-  const newUrl = new URL(
-    `https://markdown-share-button.pages.dev${url.pathname.replace(
-      "/markdown-share-button",
-      ""
-    )}${url.search}`
-  );
-  const response = await fetch(new Request(newUrl), {
-    headers: new Headers(context.request.headers),
-  });
-  // 取得したコンテンツをレスポンスとして返す
-  const responseBody = await response.blob();
-  return new Response(responseBody, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: new Headers(response.headers),
-  });
+	// /markdown-share-button/内であればhttps://markdown-share-button.pages.devよりコンテンツを取得
+	const newUrl = new URL(
+		`https://markdown-share-button.pages.dev${url.pathname.replace(
+			"/markdown-share-button",
+			""
+		)}${url.search}`
+	);
+	const response = await fetch(new Request(newUrl), {
+		headers: new Headers(context.request.headers),
+	});
+	// 取得したコンテンツをレスポンスとして返す
+	const responseBody = await response.blob();
+	return new Response(responseBody, {
+		status: response.status,
+		statusText: response.statusText,
+		headers: new Headers(response.headers),
+	});
 }
--- index.js
+++ index.js
@@ -10,17 +10,17 @@ app.use(express.static("./images"));
 app.use("/images", express.static("./images"));
 
 app.get("/", async function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/activity", async function (req, res) {
-  res.sendFile(__dirname + "/activity.html");
+	res.sendFile(__dirname + "/activity.html");
 });
 
 app.get("/style.css", async function (req, res) {
-  res.sendFile(__dirname + "/style.css");
+	res.sendFile(__dirname + "/style.css");
 });
 
 app.listen(port, function () {
-  console.log(`[NodeJS] Application Listening on Port ${port}`);
+	console.log(`[NodeJS] Application Listening on Port ${port}`);
 });
