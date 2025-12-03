9ac2b84cd3cd6565b389528944cffcfa00912a5a
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-12-03 11:03:46 +0900

    /middleware.jsのリクエスト処理を改善し、URL取得のロジックを整理

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,25 +1,21 @@
 export async function onRequest(context) {
-	const originalUrl = context.request.url;
-	const url = new URL(originalUrl);
+  const originalUrl = context.request.url;
+  const url = new URL(originalUrl);
 
-	// /markdown-share-button/内でなければ処理を中断
-	if (!url.pathname.startsWith("/markdown-share-button"))
-		return await context.next();
+  // /markdown-share-button/内でなければ処理を中断
+  if (url.pathname !== '/markdown-share-button') return await context.next();
 
-	// /markdown-share-button/内であればhttps://markdown-share-button.pages.devよりコンテンツを取得
-	// /markdown-share-button の後に続くパスを取得（最初の1回だけ置換）
-	const pathAfterPrefix = url.pathname.substring("/markdown-share-button".length);
-	const newUrl = new URL(
-		`https://markdown-share-button.pages.dev${pathAfterPrefix}${url.search}`
-	);
-	const response = await fetch(new Request(newUrl), {
-		headers: new Headers(context.request.headers),
-	});
-	// 取得したコンテンツをレスポンスとして返す
-	const responseBody = await response.blob();
-	return new Response(responseBody, {
-		status: response.status,
-		statusText: response.statusText,
-		headers: new Headers(response.headers),
-	});
+  // /markdown-share-button/内であればhttps://markdown-share-button.pages.dev よりコンテンツを取得
+  // /markdown-share-button の後に続くパスを取得（最初の1回だけ置換）
+  const newUrl = new URL('https://markdown-share-button.pages.dev');
+  const response = await fetch(new Request(newUrl), {
+    headers: new Headers(context.request.headers),
+  });
+  // 取得したコンテンツをレスポンスとして返す
+  const responseBody = await response.blob();
+  return new Response(responseBody, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: new Headers(response.headers),
+  });
 }
