5d3b1ce81692c9f1af29be510d247fa2e97fb28a
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-12-06 17:03:39 +0900

    公開用ファイルをpublicディレクトリ配下に移動し、それに伴う変更

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ let port = 80;
 // GZIP圧縮を有効にする
 app.use(compression());
 
-app.use(express.static("./images"));
-app.use("/images", express.static("./images"));
+app.use(express.static("./public/images"));
+app.use("/images", express.static("./public/images"));
 
 app.get("/", async function (req, res) {
-	res.sendFile(`${__dirname}/index.html`);
+	res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.get("/activity", async function (req, res) {
-	res.sendFile(`${__dirname}/activity.html`);
+	res.sendFile(`${__dirname}/public/activity.html`);
 });
 
 app.get("/style.css", async function (req, res) {
-	res.sendFile(`${__dirname}/style.css`);
+	res.sendFile(`${__dirname}/public/style.css`);
 });
 
 app.get("/robots.txt", async function (req, res) {
-	res.sendFile(`${__dirname}/robots.txt`);
+	res.sendFile(`${__dirname}/public/robots.txt`);
 });
 
 app.get("/sitemap.xml", async function (req, res) {
-	res.sendFile(`${__dirname}/sitemap.xml`);
+	res.sendFile(`${__dirname}/public/sitemap.xml`);
 });
 
 app.listen(port, function () {
