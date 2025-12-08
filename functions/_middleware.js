ae824195c3eb78a2b241d4e9451ba0a9fb431d53
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-12-08 21:45:48 +0900

    /middleware.jsのリクエスト処理を改善し、パスチェックの条件を明確化

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,25 +1,31 @@
 export async function onRequest(context) {
-  const originalUrl = context.request.url;
-  const url = new URL(originalUrl);
+	const originalUrl = context.request.url;
+	const url = new URL(originalUrl);
 
-  // /markdown-share-button/内でなければ処理を中断
-  if (
-    url.pathname !== '/markdown-share-button' &&
-    url.pathname !== '/markdown-share-button/'
-  )
-    return await context.next();
+	if (
+		!url.pathname.startsWith('/markdown-share-button') ||
+		url.pathname.startsWith('/markdown-share-button/markdown-share-button')
+	)
+		// /markdown-share-button/内かつ/markdown-share-button/markdown-share-button/外であれば
+		// 通常の処理を続行
+		return await context.next();
 
-  // /markdown-share-button/内であればhttps://markdown-share-button.pages.dev よりコンテンツを取得
-  // /markdown-share-button の後に続くパスを取得（最初の1回だけ置換）
-  const newUrl = new URL('https://markdown-share-button.pages.dev');
-  const response = await fetch(new Request(newUrl), {
-    headers: new Headers(context.request.headers),
-  });
-  // 取得したコンテンツをレスポンスとして返す
-  const responseBody = await response.blob();
-  return new Response(responseBody, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: new Headers(response.headers),
-  });
+	// /markdown-share-button/ または /markdown-share-button/images/ 内であれば
+	// https://markdown-share-button.pages.dev よりコンテンツを取得
+	const pathAfterPrefix = url.pathname.substring(
+		'/markdown-share-button'.length
+	);
+	const newUrl = new URL(
+		'https://markdown-share-button.pages.dev' + pathAfterPrefix + url.search
+	);
+	const response = await fetch(new Request(newUrl), {
+		headers: new Headers(context.request.headers),
+	});
+	// 取得したコンテンツをレスポンスとして返す
+	const responseBody = await response.blob();
+	return new Response(responseBody, {
+		status: response.status,
+		statusText: response.statusText,
+		headers: new Headers(response.headers),
+	});
 }
